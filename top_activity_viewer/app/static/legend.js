e308e87574c565ef1ff21f07819fa6f7df9789f1
Albert Balbekov <albert.y.balbekov@gmail.com>
2019-02-28 17:12:35 -0500

    futher improve legend item hover responsiveness by moving mouseleave handler from li to ul element

--- top_activity_viewer/app/static/legend.js
+++ top_activity_viewer/app/static/legend.js
@@ -14,40 +14,56 @@ export function defineLegend(){
 	
 // enable interaction on Legend items
 export function activateLegend(){
-	var dgUpdateOptionTimerId;
 	// for mouse over list item highlight corresponding timeseries on the chart
+	let dgUpdateOptionTimerId;
+	let highlightedWaitClassName;
+	let highlightedWaitClassID;
 	$('#legend ul')
 		.addClass("active")
 		.off('mouseenter')
 		.on( 'mouseenter', 'li', function () {
 			if (gl.gDbCredential) {
-				// highlight time series after 0.2s timeout
+			// highlight time series after 0.3s timeout
+				// save this wait class name in a closure to be visible in setTimeout callback
+				let thisWaitClassName=this.textContent;
+				let thisWaitClassID=this.id;
 				clearTimeout(dgUpdateOptionTimerId);
-				let waitClassName=this.textContent;
-				dgUpdateOptionTimerId=setTimeout(function(){
-						gl.dg.colorsMap_[waitClassName]="#FFFF00"; //yellow
-						var newOptions = {};
-						newOptions[waitClassName] = {strokeWidth: 1};
-						gl.dg.updateOptions({series: newOptions});
+				dgUpdateOptionTimerId=setTimeout(
+					function(){
+						// un-highlight previosly highlighted series
+						if (highlightedWaitClassID) {
+							gl.dg.colorsMap_[highlightedWaitClassName]=waitClassObj[highlightedWaitClassID][1];
+							gl.dg.updateOptions({strokeWidth: 1});
 						}
-					,200);
-				//gl.dg.updateOptions({series: newOptions});
-				// highlight legend item
-				//$(this).toggleClass(this.className).toggleClass("legend_highlighted");
+						// highlight this time series
+						highlightedWaitClassName=thisWaitClassName;
+						highlightedWaitClassID=thisWaitClassID;
+						gl.dg.colorsMap_[thisWaitClassName]="#FFFF00"; //yellow
+						var newOptions = {};
+						newOptions[thisWaitClassName] = {strokeWidth: 1};
+						gl.dg.updateOptions({series: newOptions});
+					}
+					,300);
 				//stop bubbling
 				return false;
 			}}
 		   )
 		.off('mouseleave')
-		.on( 'mouseleave', 'li', function () {
+		//.on( 'mouseleave', 'li', function () {
+		.on( 'mouseleave', function () {
 			if (gl.gDbCredential) {
 				// clear outstanding time series highlight request
 				clearTimeout(dgUpdateOptionTimerId);
-				// unhighlight time series if it was highlighted
-				if (gl.dg.colorsMap_[this.textContent]=="#FFFF00"){
-					gl.dg.colorsMap_[this.textContent]=waitClassObj[this.id][1];
-					gl.dg.updateOptions({strokeWidth: 1});
-				}
+				// unhighlight time series if it was highlighted, with rather long delay
+				// in a hope that it will be canceled before executing by mouseenter event
+				dgUpdateOptionTimerId=setTimeout(
+					function(){
+						if (highlightedWaitClassID){
+							gl.dg.colorsMap_[highlightedWaitClassName]=waitClassObj[highlightedWaitClassID][1];
+							gl.dg.updateOptions({strokeWidth: 1});
+						}
+					}
+					,100);
 				//gl.dg.updateOptions({strokeWidth: 1});
 				// unhighlight legend item
 				//$(this).toggleClass(this.className).toggleClass("legend_"+this.id);
